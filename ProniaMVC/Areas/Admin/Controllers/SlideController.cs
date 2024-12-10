789279b1d69e1e545ddd7903a5323a047d1129e8
Tacəddin Əsədzadə <tcddnesedzade@fmail.com>
2024-12-11 03:44:27 +0400

    Product

--- ProniaMVC/Areas/Admin/Controllers/SlideController.cs
+++ ProniaMVC/Areas/Admin/Controllers/SlideController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using ProniaMVC.Areas.Admin.ViewModel.Slides;
 using ProniaMVC.DAL;
 using ProniaMVC.Models;
 using ProniaMVC.Utilities.Extensions;
@@ -33,22 +34,32 @@ namespace ProniaMVC.Areas.Admin.Controllers
             return Content(Guid.NewGuid().ToString());
         }
         [HttpPost]
-        public async Task<IActionResult> Create(Slide slide)
+        public async Task<IActionResult> Create(CreateSlideVM slideVm)
         {
 
-            if (!slide.Photo.ValidateType("image/"))
+            if (!slideVm.Photo.ValidateType("image/"))
             {
                 ModelState.AddModelError("Photo", "File type is incoorected");
                 return View();
             }
-            if (!slide.Photo.ValidateSize(Utilities.Enums.FileSize.Mb, 2))
+            if (!slideVm.Photo.ValidateSize(Utilities.Enums.FileSize.Mb, 2))
             {
                 ModelState.AddModelError("Photo", "File size mast be less that 2 mb");
                 return View();
             }
+            Slide slide = new Slide()
+            {
+                Title = slideVm.Title,
+                SubTitle = slideVm.SubTitle,
+                Description = slideVm.Description,
+                Order = slideVm.Order,
+                Image = await slideVm.Photo.CreateFileAsync(_env.WebRootPath, "assets", "images", "website-images"),
+                IsDeleted = false,
+                CreatAt = DateTime.Now
+            };
 
 
-            slide.Image = await slide.Photo.CreateFileAsync(_env.WebRootPath, "assets", "images", "website-images");
+
 
 
 
---
+++ ProniaMVC/Areas/Admin/ViewModel/Slides/CreateSlideVM.cs
@@ -0,0 +1,16 @@
+﻿namespace ProniaMVC.Areas.Admin.ViewModel.Slides
+{
+    public class CreateSlideVM
+    {
+
+        public string Title { get; set; }
+        public string SubTitle { get; set; }
+        public string Description { get; set; }
+        public int Order { get; set; }
+        public IFormFile Photo { get; set; }
+
+
+    }
+
+}
+
